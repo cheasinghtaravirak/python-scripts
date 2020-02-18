d4e4caff5a72e20d07591d36618d89a8e715c8fb
TaraVirak <singhtaravirak@gmail.com>
2020-02-18 11:00:54 +0700

    Add files via upload

---
+++ jpg-to-png-converter.py
@@ -0,0 +1,22 @@
+import sys #for grabbing args.
+import os #for file path
+from PIL import Image #for image conversion
+
+#grab first and and second arg.
+image_folder = sys.argv[1]
+output_folder = sys.argv[2]
+
+# check if new/ exists, else create it
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+#loop through Pokedex
+for image in os.listdir(image_folder):
+    # convert each image to png and save to new folder
+    original_image = Image.open(f'{image_folder}{image}') #pokemon/pikachu.jpg
+    #extract only filename without extension
+    clean_name = os.path.splitext(image)[0] #pikachu
+    png_image = original_image.save(f'{output_folder}{clean_name}.png', 'png')
+    print('success')
+
+
